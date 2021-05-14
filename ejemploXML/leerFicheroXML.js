06370f1f8f3717d1548b77464ad9c8c948e60773
SergioPMASIR <79093256+SergioPMASIR@users.noreply.github.com>
2021-05-14 09:53:16 +0200

    Update leerFicheroXML.js

--- ejemploXML/leerFicheroXML.js
+++ ejemploXML/leerFicheroXML.js
@@ -1,5 +1,8 @@
 function gestionarFicheroXML(xmlDoc){
-	alert(xmlDoc)
+		let capaVacia = document.querySelector("#ficheroXML")
+		let alumnos = xmlDoc.querySelectorAll("libro")
+		for(let i=0; i<alumnos.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
 }
 
 loadDocA("libros.xml","xml");
