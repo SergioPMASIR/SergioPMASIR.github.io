89f84599d788ed78c0c7cd02d104b30533c5de85
SergioPMASIR <79093256+SergioPMASIR@users.noreply.github.com>
2021-05-20 18:32:02 +0200

    Update leerFicheroXML.js

--- ejemploXML/leerFicheroXML.js
+++ ejemploXML/leerFicheroXML.js
@@ -4,7 +4,8 @@ function gestionarFicheroXML(xmlDoc){
 		for(let i=0; i<libros.length; i++){
 			if(i==2){
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class = 'amarillo'>" + libros[i].textContent + "</p>"
-		} else {
+		} 
+			else {
 			capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
 		}
 }
