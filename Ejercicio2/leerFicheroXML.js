7cd2dd3680058f6686d8940c04d0314149cc9839
SergioPMASIR <79093256+SergioPMASIR@users.noreply.github.com>
2021-05-24 09:12:16 +0200

    Update leerFicheroXML.js

--- Ejercicio2/leerFicheroXML.js
+++ Ejercicio2/leerFicheroXML.js
@@ -1,7 +1,7 @@
 function gestionarFicheroXML(xmlDoc){
 		let capaVacia = document.querySelector("#ficheroXML")
-		let par = xmlDoc.querySelectorAll("par")
-		let impar = xmlDoc.querySelectorAll("impar")
+		let par = xmlDoc.querySelectorAll("pumba")
+		let impar = xmlDoc.querySelectorAll("timon")
 		for(let i = 0; i<par.length && impar.length; i++){
 			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='par'>" + par[i].textContent + "</p"
 			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='impar'>" + derecha[i].textContent + "</p>"
